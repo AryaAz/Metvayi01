49a5477fdf42e3eb8cebf7409bc78bd5b8a7e79a
Mohammad Metvayi <mohammad.metvayi@gmail.com>
2023-02-23 15:41:17 +0330

    change profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from "@mui/material";
 import {useDispatch} from "react-redux";
 import {logout} from "../../store/slice/auth.slice";
+import axios from "axios";
+import {useAppSelector} from "../../store/store";
+import {RegisterResponse} from "../../model/auth.model";
 
 const Profile = () => {
     const dispatch = useDispatch()
+
+    const [user, setUser] = useState<RegisterResponse>();
+    const token = useAppSelector(store => store.auth.accessToken)
 
     function handleLogout() {
         dispatch(logout())
     }
 
+    useEffect(() => {
+        // axios.post("url",body,{header})
+        axios.get("/api/user", {headers: {"authorization": `Bearer ${token}`}})
+            .then(res => {
+                setUser(res.data)
+            }).catch(err => {
+            alert(err.message)
+        })
+    }, [])
+
+    if (!user)
+        return <p>loading ....</p>
     return (
         <div>
             <h1>Profile</h1>
-            <p>Name : ...</p>
-            <p>Email : ...</p>
+            <p>Name : {user.name}</p>
+            <p>Email : {user.email}</p>
             <Button onClick={handleLogout}>Logout</Button>
         </div>
     );
